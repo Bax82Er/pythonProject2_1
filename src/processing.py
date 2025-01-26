46034573542b41c7e9c1fecf44108c969fd5a8b1
Батр Эрднеев <erdneevb@mail.ru>
2025-01-26 17:34:14 +0300

    init

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,29 @@
-from typing import List, Any
+def filter_by_state(transactions: list, state='EXECUTED') -> list:
+    """
+    Фильтрует список транзакций по состоянию.
+
+    :transactions: Список словарей с транзакциями.
+    :state: Состояние транзакции для фильтрации (по умолчанию 'EXECUTED').
+    :return: Новый список словарей, соответствующих переданному состоянию.
+    """
+    return [transaction for transaction in transactions if transaction['state'] == state]
 
 
-def filter_by_state(data, state='EXECUTED'):
-    """Возвращает список словарей, соответствующих состоянию."""
-    return [item for item in data if item['state'] == state]
+from datetime import datetime
 
 
-def sort_by_date(data, order='descending'):
-    """Сортирует список словарей по дате."""
-    sorted_data: list[Any] = sorted(data, key=lambda x: x['date'], reverse=order == 'descending')
-    return sorted_data
+def sort_by_date(transactions: list, descending=True) -> list:
+    """
+    Сортирует список транзакций по дате.
+
+    :transactions: Список словарей с транзакциями.
+    :descending: Порядок сортировки (по умолчанию True - по убыванию).
+    :return: Отсортированный список словарей.
+    """
+
+    def get_date(transaction):
+        return datetime.strptime(transaction['date'], '%Y-%m-%dT%H:%M:%S.%f')
+
+    sorted_transactions = sorted(transactions, key=get_date, reverse=descending)
+    return sorted_transactions
+
