e44bb219b04a23e9ebe79dff927b83a8b0fd06dd
Батр Эрднеев <erdneevb@mail.ru>
2025-02-11 15:00:32 +0300

    'fixes API'

--- src/external_api.py
+++ src/external_api.py
@@ -5,7 +5,7 @@ import requests
 load_dotenv()  # Загружаем переменные окружения из .env
 
 API_KEY = os.getenv('API_KEY')
-BASE_URL = 'http://api.exchangeratesapi.io/v1/latest'
+BASE_URL = 'https://api.apilayer.com/exchange_rates'
 
 
 def convert_to_rub(transaction):
@@ -21,15 +21,15 @@ def convert_to_rub(transaction):
     if currency == 'RUB':
         return float(amount)
 
-    response = requests.get(
-        BASE_URL,
-        params={'access_key': API_KEY}
-    )
+    headers = {"apikey": API_KEY}
+    params = {"source": currency, "currencies": "RUB"}
+
+    response = requests.get(BASE_URL, headers=headers, params=params)
 
     if response.status_code != 200:
         raise Exception(f'Не удалось получить курс валют: {response.text}')
 
-    rates = response.json()['rates']
-    rate = rates[currency]
+    rates = response.json()['quotes']
+    rate = rates[f'{currency}RUB']
 
     return round(float(amount) * rate, 2)
